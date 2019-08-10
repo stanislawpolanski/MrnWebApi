d68b76e063708629c6e6b195cf207e69aad1b7af
Staszek <stanislaw.polanski@hotmail.com>
2019-08-10 18:54:01 +0200

    #82 Station endpoints tests stub created

---
+++ UnitTests/EndpointsTests/StationEndpointsTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTests.EndpointsTests
+{
+    public class StationEndpointsTests
+    {
+        [Fact]
+        public void GetAllTheStations()
+        {
+            throw new Xunit.Sdk.XunitException("test not developed");
+        }
+        [Fact]
+        public void GetSingleStation()
+        {
+            throw new Xunit.Sdk.XunitException("test not developed");
+        }
+        [Fact]
+        public void PostStation()
+        {
+            throw new Xunit.Sdk.XunitException("test not developed");
+        }
+        [Fact]
+        public void PutStation()
+        {
+            throw new Xunit.Sdk.XunitException("test not developed");
+        }
+        [Fact]
+        public void DeleteStation()
+        {
+            throw new Xunit.Sdk.XunitException("test not developed");
+        }
+    }
+}
