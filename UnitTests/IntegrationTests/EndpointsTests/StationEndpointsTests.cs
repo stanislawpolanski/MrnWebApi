fd9b8331af62b5c6becc655b8108f41e2650ebeb
Staszek <stanislaw.polanski@hotmail.com>
2019-08-11 11:34:47 +0200

    #82 All planned GET tests for Station developed

--- UnitTests/IntegrationTests/EndpointsTests/StationEndpointsTests.cs
+++ UnitTests/IntegrationTests/EndpointsTests/StationEndpointsTests.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Xunit;
-using MrnWebApi.Controllers;
-using MrnWebApi.Logic.StationService;
-using MrnWebApi.DataAccess.ServicesFactory;
-using Microsoft.AspNetCore.Mvc.Testing;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
+﻿using Microsoft.AspNetCore.Mvc.Testing;
 using MrnWebApi.Common.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace UnitTests.IntegrationTests.EndpointsTests
 {
@@ -32,9 +28,8 @@ namespace UnitTests.IntegrationTests.EndpointsTests
         {
             //arrange
             int expectedNumberOfStations = 200;
-            var client = factory.CreateClient();
             //act
-            var response = await client.GetAsync(url);
+            var response = await GetResponseByUrl(url);
             List<StationModel> models = 
                 await GetStationsListFromResponse(response);
             //assert
@@ -59,12 +54,11 @@ namespace UnitTests.IntegrationTests.EndpointsTests
             GetAllStations_AllStationsContainsNameLongerThanSpecifiedNumber(string url)
         {
             //arrange
-            var client = factory.CreateClient();
             int specifiedNameLength = 3;
             Action<StationModel> stationNameLongerThanSpecifiedNumber = 
                 model => Assert.True(model.Name.Length > specifiedNameLength);
             //act
-            var response = await client.GetAsync(url);
+            var response = await GetResponseByUrl(url);
             List<StationModel> models =
                 await GetStationsListFromResponse(response);
             //assert
@@ -82,18 +76,95 @@ namespace UnitTests.IntegrationTests.EndpointsTests
                 model => Assert
                     .Equal(model.Url, endpointUrl + "/" + model.Id.ToString());
             //act
-            var response = await client.GetAsync(endpointUrl);
+            var response = await GetResponseByUrl(endpointUrl);
             List<StationModel> models =
                 await GetStationsListFromResponse(response);
             //assert 
-            models
-                .ForEach(actualUrlEqualsExpectedUrl);
+            models.ForEach(actualUrlEqualsExpectedUrl);
         }
-        [Fact]
-        public void GetSingleStation()
+
+        [Theory]
+        [InlineData("/api/station/78", "Trzebinia")]
+        [InlineData("api/station/10112","Łęg Tarnowski")]
+        public async Task 
+            GetStationById_ReturnsStationWithExpectedName
+                (string url, string expectedName)
         {
-            throw new Xunit.Sdk.XunitException("test not developed");
+            //act
+            StationModel station = 
+                await GetDeserialisedTextFromResponseByUrl<StationModel>(url);
+            //assert
+            Assert.Equal(expectedName, station.Name);
         }
+
+        private async Task<T> 
+            GetDeserialisedTextFromResponseByUrl<T>(string url)
+        {
+            HttpResponseMessage response = await GetResponseByUrl(url);
+            var text = await response.Content.ReadAsStringAsync();
+            T result = DeserialiseJson<T>(text);
+            return result;
+        }
+
+        private static T DeserialiseJson<T>(string text)
+        {
+            return JsonConvert.DeserializeObject<T>(text);
+        }
+
+        private async Task<HttpResponseMessage> GetResponseByUrl(string url)
+        {
+            var client = factory.CreateClient();
+            var response = await client.GetAsync(url);
+            return response;
+        }
+
+        
+        [Theory]
+        [InlineData("/api/station/78", 5)]
+        public async Task 
+            GetStationById_ReturnsStationWithRailwaysAsync(string url, 
+                int expectedNumberOfRailways)
+        {
+            StationModel model =
+                await GetDeserialisedTextFromResponseByUrl<StationModel>(url);
+            Assert.Equal(expectedNumberOfRailways, model.Railways.Count());
+        }
+        
+        [Theory]
+        [InlineData("api/station/78", 16)]
+        public async Task GetStationById_ReturnsStationWithPhotosAsync(string url, 
+            int expectedNumberOfPhotos)
+        {
+            StationModel model =
+                await GetDeserialisedTextFromResponseByUrl<StationModel>(url);
+            Assert.Equal(expectedNumberOfPhotos, model.Photos.Count());
+        }
+
+        
+        [Theory]
+        [InlineData("api/station/78", "POINT (532345.109 254186.948)")]
+        public async Task GetStationById_ReturnsStationWithGeometry(string url, 
+            string expectedGeometryValue)
+        {
+            StationModel model =
+                await GetDeserialisedTextFromResponseByUrl<StationModel>(url);
+
+            Assert.Equal(expectedGeometryValue, 
+                model.SerialisedGeometry.SerialisedSpatialData);
+        }
+        
+        [Theory]
+        [InlineData("api/station/78", "KRAKÓW")]
+        public async Task 
+            GetStationById_ReturnsStationWithRailwayUnitAsync(string url,
+                string expectedRailwayUnitName)
+        {
+            StationModel model =
+                await GetDeserialisedTextFromResponseByUrl<StationModel>(url);
+
+            Assert.Equal(expectedRailwayUnitName, model.RailwayUnit.Name);
+        }
+
         [Fact]
         public void PostStation()
         {
