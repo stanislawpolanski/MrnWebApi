d5966740df9b9f28d18d8773e97004ef2b8867dd
Staszek <stanislaw.polanski@hotmail.com>
2019-09-01 19:22:52 +0200

    #111 Test stubs for get single railway usecase

---
+++ DatabaseAPI.Tests/IntegrationTests/EndpointsTests/Railway/GetSingleRailwayTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DatabaseAPI.Tests.IntegrationTests.EndpointsTests.Railway
+{
+    public class GetSingleRailwayTests
+        : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<DatabaseAPI.Startup> factory;
+
+        public GetSingleRailwayTests(
+            WebApplicationFactory<DatabaseAPI.Startup> injectedFactory)
+        {
+            factory = injectedFactory;
+        }
+
+        [Theory]
+        [InlineData("/database-api/railway/2")]
+        [InlineData("/database-api/railway/23")]
+        public async Task Returns200OnExistingRailway(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Theory]
+        [InlineData("/database-api/railway/123456789")]
+        public async Task Returns404OnNonExistingRailway(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Theory]
+        [InlineData("/database-api/railway/23")]
+        public async Task ReturnsNameOfTheRailway(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Theory]
+        [InlineData("/database-api/railway/23")]
+        public async Task ReturnsNumberOfTheRailway(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Theory]
+        [InlineData("/database-api/railway/23")]
+        public async Task ReturnsOwnerOfTheRailway(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Theory]
+        [InlineData("/database-api/railway/23")]
+        public async Task ReturnsStationsWithKmPostsForRailway(string url)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
