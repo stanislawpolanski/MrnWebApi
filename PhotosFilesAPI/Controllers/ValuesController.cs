49005614d7e48e80567648d4d11e0f9d16e2469f
Staszek <stanislaw.polanski@hotmail.com>
2019-10-19 17:31:30 +0200

    #26 Photo API now runnable

--- PhotosFilesAPI/Controllers/ValuesController.cs
+++ PhotosFilesAPI/Controllers/ValuesController.cs
@@ -1,8 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace PhotosFilesAPI.Controllers
 {
@@ -12,34 +9,9 @@ namespace PhotosFilesAPI.Controllers
     {
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<string>> GetFileNames()
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return new string[] { "value1.jpg", "value2.jpg" };
         }
     }
 }
