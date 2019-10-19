3c4e4383db19c1c9702f098d3c2bd1b3e986845f
Staszek <stanislaw.polanski@hotmail.com>
2019-10-19 17:59:30 +0200

    #26 List of files readable

--- PhotosFilesAPI/Startup.cs
+++ PhotosFilesAPI/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PhotosFilesAPI.Services;
 
 namespace PhotosFilesAPI
 {
@@ -19,6 +20,7 @@ namespace PhotosFilesAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddTransient<IPhotosService, PhotosFilesService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
